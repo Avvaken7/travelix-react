5571f3bf1b10f1c7330406ddba41e567fd27f7ea
Slava <avvaken7@gmail.com>
2022-11-09 21:22:00 +0200

    add mainHeader + search

--- src/components/header/mainHeader/MainHeader.jsx
+++ src/components/header/mainHeader/MainHeader.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
+import './mainHeader.css';
+
 
 const MainHeader = () => {
+    return (
+        <main>
+            <div className="square"></div>
+            <div className="main_text">
+                <h1 className="main_text-title">Happiness Is <span>Travelling</span></h1>
+                <h2 className="main_text-subtitle">No matter where in the world you want to go, we can help get you there</h2>
+                <p className="main_text-description">Travel Agency Threats and Opportunities. The Perspective of Successful OwnersJoin the leader in small-ship cruising on the Great Lakes, and enjoy fabulous inclusions such as a pre-voyage hotel.</p>
+            </div>
+            <div className="main_img">
+                <img src="../images/sun_center.png" alt="sun" />
+            </div>
+        </main>
 
+    )
 }
 
 export default MainHeader;
--- src/components/header/search/Search.jsx
+++ src/components/header/search/Search.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import "./search.css"
 
 const Search = () => {
+
+    return (
+        <fieldset>
+            <form action="#">
+                <div className="form_group">
+                    <label htmlFor="location">Location<br/>
+                        <input type="text" id="location" name="location" />
+                    </label>
+
+                    <label htmlFor="in">Check in<br/>
+                        <input type="date" id="in" name="in" placeholder="Add dates" />
+                    </label>                   
+                    <label htmlFor="out">Check out<br/>
+                        <input type="date" id="out" name="out" placeholder="Add dates" />
+                    </label>                    
+                    <input type="submit" />
+                </div>
+            </form>
+        </fieldset>
+    )
+
 
 }
 
